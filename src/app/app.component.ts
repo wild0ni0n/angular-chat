f4de633fe5e6904cb620ae61d0811e859ca4e5b9
wild0ni0n <yuisuwako@gmail.com>
2023-01-09 18:25:24 +0900

    create mock

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
 
+import { Comment } from './class/comment';
+import { User } from './class/user';
+
+const CURRENT_USER: User = new User(1, '五十嵐 洋平');
+const ANOTHER_USER: User = new User(2, "竹内 健司");
+
+const COMMENTS: Comment[] = [
+  new Comment(ANOTHER_USER, 'お疲れ様です。'),
+  new Comment(ANOTHER_USER, 'この間の件ですが、どうなりましたか？'),
+  new Comment(CURRENT_USER, 'お疲れ様です。'),
+  new Comment(CURRENT_USER, 'クライアントからOKが出ました。'),]
+
 @Component({
   selector: 'ac-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-chat';
+  comments = COMMENTS;
+  currentUser = CURRENT_USER;
 }
---
+++ src/app/class/comment.ts
@@ -0,0 +1,5 @@
+import { User } from './user';
+export class Comment {
+
+    constructor(public user: User, public message: string) { }
+}
