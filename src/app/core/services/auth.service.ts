e76c304b02c52365bc837e402a91ce1f15f3e231
wild0ni0n <yuisuwako@gmail.com>
2023-01-12 16:45:13 +0900

    add auth service

---
+++ src/app/core/services/auth.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private afAuth: AngularFireAuth) { }
+
+  create(email: string, password: string): Promise<void> {
+    return this.afAuth.createUserWithEmailAndPassword(email, password)
+      .then((credential) => {
+        const { user } = credential;
+        const actionCodeSettings = {
+          url: `http://localhost:4200/?newAccount=true&email=${user?.email}`
+        };
+        user?.sendEmailVerification(actionCodeSettings)
+      });
+  }
+  login(email: string, password: string): Promise<firebase.default.auth.UserCredential | void> {
+    return this.afAuth.signInWithEmailAndPassword(email, password)
+      .catch(error => console.error(error));
+  }
+  logout(): Promise<void> {
+    return this.afAuth.signOut();
+  }
+}
