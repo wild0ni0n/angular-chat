4ab91c8005051aa534f432b0c6920873d4c0e95a
wild0ni0n <yuisuwako@gmail.com>
2023-01-13 16:41:16 +0900

    add authguard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,13 @@ import { ChatComponent } from './chat/chat.component';
 import { NotFoundComponent } from './core/components/not-found/not-found.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { LoginComponent } from './login/login.component';
+import { AuthGuard } from './core/guards/auth.guard';
 
 const routes: Routes = [
   { path: '', component: ChatComponent },
   { path: 'users', loadChildren: () => import('./users/users.module').then(m => m.UsersModule) },
-  { path: 'signup', component: SignUpComponent },
-  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignUpComponent, canActivate: [AuthGuard] },
+  { path: 'login', component: LoginComponent, canActivate: [AuthGuard] },
   { path: '**', component: NotFoundComponent },
 ];
 
---
+++ src/app/core/guards/auth.guard.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+
+  constructor(
+    private afAuth: AngularFireAuth,
+    private router: Router
+  ) { }
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+
+    return this.afAuth.authState.pipe(
+      map((user) => {
+        if (!user) {
+          return true;
+        } else {
+          // this.router.navigateByUrl('/');
+          // return false;
+          console.log(this.router.parseUrl('/'));
+          return this.router.parseUrl('/');
+        }
+      })
+    )
+  }
+
+}
